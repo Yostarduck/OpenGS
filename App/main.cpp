9e0e440499eabfc228f737d25cca58131cc4e616
Francisco <yostarduck@gmail.com>
2025-02-09 02:00:17 -0600

    + Added FreeImage submodule

--- App/main.cpp
+++ App/main.cpp
@@ -3,10 +3,15 @@
 #include "AppConfig.h"
 #include "FMath.h"
 
+#include "SDL.h"
+
 int
 main(int args, char **argv) {
   std::cout << argv[0] << " version: " << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << std::endl;
-  std::cout << "sqrt(2) = " << FMath::sqrt(2) << std::endl;
   
+  SDL_Log("SDL version: %d.%d.%d", SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_MICRO_VERSION);
+  
+  std::cout << "sqrt(2) = " << FMath::sqrt(2) << std::endl;
+
   return 0;
 }
