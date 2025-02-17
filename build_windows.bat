b4def79e780ea871fe5a142f4c5554041de2a56d
Francisco <yostarduck@gmail.com>
2025-02-17 15:04:26 -0600

    * Fixed windows build file not working correctly

--- build_windows.bat
+++ build_windows.bat
@@ -3,13 +3,15 @@ setlocal enabledelayedexpansion
 
 set BUILD_TYPE=Debug
 
-if not "%1"=="" (
-    set ARG=%1
-    set ARG=!ARG:"=!
-    if /I "%ARG%"=="Release" set BUILD_TYPE=Release
-    if /I "%ARG%"=="Debug" set BUILD_TYPE=Debug
-    else (
-        echo Invalid Build Type: %ARG%
+if not "%~1"=="" (
+    set ARG=%~1
+
+    if /I "!ARG!"=="Release" (
+        set BUILD_TYPE=Release
+    ) else if /I "!ARG!"=="Debug" (
+        set BUILD_TYPE=Debug
+    ) else (
+        echo Invalid Build Type: !ARG!
         echo Valid Build Types: Debug, Release
         echo Defaulting to Debug
     )
@@ -17,7 +19,7 @@ if not "%1"=="" (
 
 echo Selected Build Type: %BUILD_TYPE%
 
-rm -rf Build
+rmdir /s /q Build 2>nul
 
 cmake -B Build -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 cmake --build Build --config %BUILD_TYPE%
